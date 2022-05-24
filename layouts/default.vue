a26eb4d0b0dd4b66aa3187354530b3d5e1a719b3
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-05-24 14:40:36 +0700

    push

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <nuxt keep-alive />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    {{ count }}
+    <button @click="count++">increase</button>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    -> current scroll position here (this scroll position will be retained) <-
+    <br>
+    
+    <nuxt-link :to="'another'">go to another page</nuxt-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+}
+</script>
